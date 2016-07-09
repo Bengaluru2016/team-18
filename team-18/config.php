f339bc933518cd7d96d8d6a7aa87d1f9d5484559
sujata007 <priyadarshinee11@gmail.com>
2016-07-10 03:36:36 +0530

    config.php

---
+++ team-18/config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'sujata');
+   define('DB_DATABASE', 'rangde');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
